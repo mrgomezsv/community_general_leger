f5ccf90f00f45bd7df0a070985b7aa8d18eb4d52
Segio Hernandez <innovations.techology@gmail.com>
2024-08-03 23:06:53 -0600

    Se agrego el campo company_id

--- models/account_group.py
+++ models/account_group.py
@@ -5,4 +5,5 @@ from odoo import models, fields
 class AccountGroup(models.Model):
     _inherit = 'account.group'
 
+    company_id = fields.Many2one('res.company', string="Compañia", default=lambda self: self.env.company)
     major_account = fields.Boolean(string="Es cuenta de mayor?")
